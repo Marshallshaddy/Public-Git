831fcb7a8abd99dcffc687f22e65beca35760a90
Marshallshaddy <marshallshaddy6@gmail.com>
2023-10-29 16:53:05 +0100

    An Advanced Weather Program with OpenWeatherMap API Integration and a Basic TKinter UI"
    This commit introduces a feature-rich weather application that harnesses the power of the OpenWeatherMap API. Users can input a city name and access an array of real-time data, including temperature, humidity, and wind speed.
    
    The included API key is: 9229757a39bd973a1efb207a3443a65e (My OpenWeatherMap API). While this key is available for free, it comes with a limited daily request allocation. Users with greater requirements can opt for a paid API key.
    
    To use the program, simply input a city name in the text field and click the "Get Weather" button. The program will promptly provide a comprehensive set of current weather data for the specified city.
    
    Changes in this commit:
    - Development of an advanced weather program with robust features.
    - Integration of the OpenWeatherMap API for extensive weather data retrieval.
    - Basic TKinter UI for user interaction and data display.
    
    Changes to be committed:
    - Modified:   Single script projects/AdvWeatherApp.py

--- Single script projects/AdvWeatherApp.py
+++ Single script projects/AdvWeatherApp.py
@@ -1,79 +1,58 @@
 import json
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
+import requests
 
 def get_weather_data(city):
-    url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=API_KEY&units=metric&humidity&wind&uvi"
-    response = requests.get(url)
-    data = response.json()
-    return data
-
-def save_weather_data(city):
-    weather_data = get_weather_data(city)
-    with open('weather_data.json', 'w') as f:
-        json.dump(weather_data, f)
+    try:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=9229757a39bd973a1efb207a3443a65e&units=metric"
+        response = requests.get(url)
+        data = response.json()
+        if "message" in data:
+            print(f"Error retrieving data from OpenWeatherMap: {data['message']}")
+            return None
+        return data
+    except Exception as e:
+        print(f"An error occurred while retrieving the weather data: {e}")
+        return None
 
 def display_weather_data():
+    global city_entry
     city = city_entry.get()
     weather_data = get_weather_data(city)
-
-    # Create a new window to display the weather data
-    weather_window = tk.Toplevel()
-    weather_window.title("Weather Data")
-
-    # Create a frame to hold the weather data
-    weather_frame = ttk.Frame(weather_window)
-
-    # Create a label for each piece of weather data
-    temperature_label = ttk.Label(weather_frame, text="Temperature:")
-    humidity_label = ttk.Label(weather_frame, text="Humidity:")
-    wind_label = ttk.Label(weather_frame, text="Wind:")
-    uvi_label = ttk.Label(weather_frame, text="UV Index:")
-
-    # Create a text box for each piece of weather data
-    temperature_text = ttk.Entry(weather_frame, width=10)
-    humidity_text = ttk.Entry(weather_frame, width=10)
-    wind_text = ttk.Entry(weather_frame, width=10)
-    uvi_text = ttk.Entry(weather_frame, width=10)
-
-    # Place the labels and text boxes on the frame
-    temperature_label.grid(row=0, column=0)
-    temperature_text.grid(row=0, column=1)
-    humidity_label.grid(row=1, column=0)
-    humidity_text.grid(row=1, column=1)
-    wind_label.grid(row=2, column=0)
-    wind_text.grid(row=2, column=1)
-    uvi_label.grid(row=3, column=0)
-    uvi_text.grid(row=3, column=1)
-
-    # Pack the frame into the window
-    weather_frame.pack()
-
-    # Display the window
-    weather_window.mainloop()
-
+    if weather_data is not None:
+        weather_window = tk.Toplevel()
+        weather_window.title("Weather Data")
+        weather_frame = ttk.Frame(weather_window)
+        temperature_label = ttk.Label(weather_frame, text="Temperature:")
+        humidity_label = ttk.Label(weather_frame, text="Humidity:")
+        wind_label = ttk.Label(weather_frame, text="Wind:")
+        temperature_text = ttk.Label(weather_frame, text=weather_data['main']['temp'])
+        humidity_text = ttk.Label(weather_frame, text=weather_data['main']['humidity'])
+        wind_text = ttk.Label(weather_frame, text=weather_data['wind']['speed'])
+        temperature_label.grid(row=0, column=0)
+        temperature_text.grid(row=0, column=1)
+        humidity_label.grid(row=1, column=0)
+        humidity_text.grid(row=1, column=1)
+        wind_label.grid(row=2, column=0)
+        wind_text.grid(row=2, column=1)
+        weather_frame.pack()
+        weather_window.mainloop()
+    else:
+        messagebox.showerror("Error", "Invalid city name or other error occurred.")
 
 def main():
     root = tk.Tk()
     root.title("Weather App")
-
-    # Create a label for the city entry
-    city_label = tk.Label(root, text="Enter city name:")
-
-    # Create an entry for the city name
+    global city_entry
+    city_label = tk.Label(root, text="Enter city name")
     city_entry = tk.Entry(root)
-
-    # Create a button to get the weather data
     get_weather_button = tk.Button(root, text="Get Weather", command=display_weather_data)
-
-    # Place the label, entry, and button on the root window
     city_label.pack()
     city_entry.pack()
     get_weather_button.pack()
-
-    # Start the mainloop
     root.mainloop()
-
 
 if __name__ == "__main__":
     main()
